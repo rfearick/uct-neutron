11ecbc6daae121ee23fdb53c2cbaf6c1b290df3d
Roger Fearick <Roger.Fearick@uct.ac.za>
2017-11-29 16:49:09 +0200

    add a filefield and code to get file names

--- gui/fileentry.py
+++ gui/fileentry.py
@@ -1,15 +1,57 @@
+from pathlib import Path
+
 from PyQt5 import Qt, QtCore, QtWidgets, QtGui
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 
 from PyQt5.QtWidgets import QWidget, QTabWidget, QVBoxLayout, QHBoxLayout, QLabel
 from PyQt5.QtWidgets import QLineEdit
 
+"""
+Gather the file names needed for analysis
+"""
+
+class FileField(QLineEdit):
+    """
+    Subclass the QLineEdit to repurpose it for file field duty.
+
+    Double click will bring up the file dialog.
+    This widget uses the standard python pathlib to parse and manipulate file
+    paths, in an OS independent way.
+    """
+    currentpath=None
+    def __init__(self):
+        super().__init__()
+        self.filename=None
+
+    def mouseDoubleClickEvent(self, event):      
+        self.getFile()
+
+    def getFile(self):
+        directory=FileField.currentpath if FileField.currentpath is not None else '.'
+        directory=str(directory)
+        #print("directory", directory)
+        filename,_=Qt.QFileDialog.getOpenFileName(self,'Open file',directory,"List files (*.lst)")
+        print(filename)
+        if filename == '': return
+        pp=Path(filename)
+        if pp.exists():
+            self.filename=filename
+            self.path=pp
+            self.name=pp.name
+            self.stem=pp.stem
+            self.parentpath=pp.parent
+            if FileField.currentpath is None or FileField.currentpath != self.parentpath:
+                FileField.currentpath=pp.parent           
+            self.setText(pp.name)
+            #print("input file:",filename)
+            #print("parent    :",pp.parent)
+            mpapath=pp.with_suffix(".mpa")
+            #print("mpafile   :",mpapath.exists())
 
 class FilePicker(QTabWidget):
     """
     Create a tabbed widget with file entry points for calibration, ne213 and fc
     """
-
     def __init__(self):
 
         super().__init__()
@@ -21,37 +63,49 @@ class FilePicker(QTabWidget):
         self._makeNE213Tab()
         self._makeFCTab()
 
+        self.editNa=None
+        self.editCs=None
+        self.editAmBe=None
+        self.editTAC=None
+        self.editNE213=None
+        self.editFC=None
+        
     def _makeCalibTab(self):
 
         layout=QVBoxLayout()
         hlayout=QHBoxLayout()
         hlayout.addWidget( QLabel("22Na") )
-        hlayout.addWidget( QLineEdit() )
+        self.editNa=FileField()
+        hlayout.addWidget( self.editNa )
         layout.addLayout( hlayout )
         
         hlayout=QHBoxLayout()
         hlayout.addWidget( QLabel("137Cs") )
-        hlayout.addWidget( QLineEdit() )
+        self.editCs=FileField()
+        hlayout.addWidget( self.editCs )
         layout.addLayout( hlayout )
        
         hlayout=QHBoxLayout()
         hlayout.addWidget( QLabel("AmBe") )
-        hlayout.addWidget( QLineEdit() )
+        self.editAmBe=FileField()
+        hlayout.addWidget( self.editAmBe )
         layout.addLayout( hlayout )
         
         hlayout=QHBoxLayout()
         hlayout.addWidget( QLabel("TAC") )
-        hlayout.addWidget( QLineEdit() )
+        self.editTAC=FileField()
+        hlayout.addWidget( self.editTAC )
         layout.addLayout( hlayout )
 
         self.calibfiles.setLayout(layout)
         self.addTab( self.calibfiles, "Calibration" )
-        
+
     def _makeNE213Tab(self):
         layout=QVBoxLayout()
         hlayout=QHBoxLayout()
         hlayout.addWidget( QLabel("NE213") )
-        hlayout.addWidget( QLineEdit() )
+        self.editNE213=FileField()
+        hlayout.addWidget( self.editNE213 )
         layout.addLayout( hlayout )
         self.ne213files.setLayout(layout)
         self.addTab( self.ne213files, "NE213" )
@@ -60,7 +114,8 @@ class FilePicker(QTabWidget):
         layout=QVBoxLayout()
         hlayout=QHBoxLayout()
         hlayout.addWidget( QLabel("Fission") )
-        hlayout.addWidget( QLineEdit() )
+        self.editFC=FileField()
+        hlayout.addWidget( self.editFC )
         layout.addLayout( hlayout )
         self.fcfiles.setLayout(layout)
         self.addTab( self.fcfiles, "Fission Chamber" )
